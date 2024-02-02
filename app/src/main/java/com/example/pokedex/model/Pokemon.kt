9610dae0f84186e41ea1c27eb09d7e1849919f1c
Orlandroid <lopez.orlando.gold9@gmail.com>
2024-02-02 12:12:57 -0600

    [FIX] solve isue when light_color_code come null and make that the app crash

--- app/src/main/java/com/example/pokedex/model/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -9,9 +9,9 @@ data class Pokemon(
     val num: String,
     val name: String,
     val img: String,
-    val light_color_code : String,
+    val light_color_code: String?,
     val type: List<String>,
     val height: String,
     val weight: String,
     val next_evolution: String?
-): Parcelable
+) : Parcelable
